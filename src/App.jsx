e3fc6ef01f3a84b4c80a7302108e2b3b6248db9a
Eliska Janovcova <elissx99@gmail.com>
2026-06-14 18:02:34 +0200

    First base of project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Moje UX/UI Portfolio</h1>
+      <p>Ahoj, jsem designérka a tohle je můj budoucí web.</p>
+    </div>
+  )
+}
+
+export default App
